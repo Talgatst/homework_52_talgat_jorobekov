4d19cb7781e0aded24682805ffb4f39a7c23a53f
Talgat <JorobekovT@gmail.com>
2025-11-06 07:27:30 +0600

    создал модель Задач

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+from django.db import models
+
+status_choices = [
+    ('new', 'Новая'),
+    ('in_progress', 'В процессе'),
+    ('done', 'Сделано'),
+]
+
+
+class Task(models.Model):
+    description = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        verbose_name="Описание задачи"
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=status_choices,
+        default='new',
+        verbose_name="Статус"
+    )
+    deadline_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name="Дата выполнения"
+    )
+
+    def __str__(self):
+        return self.description
